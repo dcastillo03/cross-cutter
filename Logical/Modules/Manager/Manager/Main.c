cb9e52493e6095f12aa9e7129c3a8d7ff92b73bc
Daniel Castillo-Idrobo <didrobo@i-automation.com>
2025-10-15 10:36:26 -0400

    first commit 10/15

--- Logical/Modules/Manager/Manager/Main.c
+++ Logical/Modules/Manager/Manager/Main.c
@@ -12,6 +12,10 @@ void _INIT ProgramInit(void)
 	Conveyor.Parameters = &ConveyorPar;
 	Conveyor.Enable = 1;
 	
+	ConveyorPar.Velocity = 45;
+	ConveyorPar.Acceleration = 1000;
+	ConveyorPar.Deceleration = 1000;
+	
 	Slicer.MpLink = &Slave;
 	Slicer.Parameters = &SlicerPar;
 	Slicer.Enable = 1;
@@ -23,22 +27,40 @@ void _INIT ProgramInit(void)
 	
 	TouchProbe.Enable = 1;
 	TouchProbe.Axis = &Master;
+	TouchProbe.TriggerInput.EventSource = mcEVENT_SRC_TRIGGER1;
+	TouchProbe.Period = 140.0;
 	
 
 }
 
 void _CYCLIC ProgramCyclic(void)
 {
-//	if (!Conveyor.PowerOn && Conveyor.Active) {
-//		Conveyor.Power = 1;
-//	}
-//	
-//	if (!Slicer.PowerOn && Slicer.Active) {
-//		Slicer.Power = 1;
-//	}
+	if (!Conveyor.PowerOn && Conveyor.Active) {
+		Conveyor.Power = 1;
+	}
 	
+	if (!Slicer.PowerOn && Slicer.Active) {
+		Slicer.Power = 1;
+	}
+	
+	if (Conveyor.PowerOn && !Conveyor.IsHomed) {
+		Conveyor.Home = 1;
+	}
+
+	if (Slicer.PowerOn && !Slicer.IsHomed) {
+		Slicer.Home = 1;
+	}
+	
+	if (TouchProbe.ValidTriggerCount > PrevCounter) {
+		Pulse = 1;
+	} else {
+		Pulse = 0;
+	}
 
 	
+	
+
+	PrevCounter = TouchProbe.ValidTriggerCount;
 	MpAxisBasic(&Conveyor);
 	MpAxisBasic(&Slicer);
 	MpAxisCamSequencer(&Sequencer);
