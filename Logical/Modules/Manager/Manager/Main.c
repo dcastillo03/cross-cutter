b6603fe854ece5ae7c4f8e95b9a660999631dc20
Daniel Castillo-Idrobo <didrobo@i-automation.com>
2025-10-15 17:59:56 -0400

    3rd commit 10/15

--- Logical/Modules/Manager/Manager/Main.c
+++ Logical/Modules/Manager/Manager/Main.c
@@ -12,7 +12,7 @@ void _INIT ProgramInit(void)
 	Conveyor.Parameters = &ConveyorPar;
 	Conveyor.Enable = 1;
 	
-	ConveyorPar.Velocity = 45;
+	ConveyorPar.Velocity = 65;
 	ConveyorPar.Acceleration = 1000;
 	ConveyorPar.Deceleration = 1000;
 	
@@ -35,6 +35,7 @@ void _INIT ProgramInit(void)
 
 void _CYCLIC ProgramCyclic(void)
 {
+	// Power on conveyor and slicer if FUBs are active
 	if (!Conveyor.PowerOn && Conveyor.Active) {
 		Conveyor.Power = 1;
 	}
@@ -43,6 +44,7 @@ void _CYCLIC ProgramCyclic(void)
 		Slicer.Power = 1;
 	}
 	
+	// Home conveyor and slicer if they are powered
 	if (Conveyor.PowerOn && !Conveyor.IsHomed) {
 		Conveyor.Home = 1;
 	}
@@ -51,18 +53,35 @@ void _CYCLIC ProgramCyclic(void)
 		Slicer.Home = 1;
 	}
 	
+	// Move velocity conveyor once homed
+	if (Conveyor.IsHomed && Slicer.IsHomed) {
+		Conveyor.MoveVelocity = 1;
+		Sequencer.StartSequence = 1;
+	}
+	
 	if (TouchProbe.ValidTriggerCount > PrevCounter) {
 		Pulse = 1;
-		if (Pulse) {
-			Sequencer.Signal1 = 1;
+		
+		switch (Sequencer.ActualStateIndex)
+		{
+			case 0:
+				Sequencer.Signal1 = 1;
+				break;
+			case 1:
+				Sequencer.Signal2 = 1;
+				break;
+			case 2:
+				Sequencer.Signal3 = 1;
+				break;
+			
 		}
 	} else {
 		Pulse = 0;
-		Sequencer.Signal1 = 0;
 	}
-	
 
-	
+	if (Sequencer.ActualStateIndex = 2 && Sequencer.InCompensation) {
+		Sequencer.Signal3 = 0;
+	}
 	
 	// Assignments
 	PrevCounter = TouchProbe.ValidTriggerCount;
