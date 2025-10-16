edd0fc2d3d7b1b535602108ad4e91905d710c53a
Daniel Castillo-Idrobo <didrobo@i-automation.com>
2025-10-16 14:49:42 -0400

    1st commit 10/16

--- Logical/Modules/Manager/Manager/Main.c
+++ Logical/Modules/Manager/Manager/Main.c
@@ -7,15 +7,16 @@
 
 void _INIT ProgramInit(void)
 {
-	
+	// Initializiation of Conveyor MpAxis
 	Conveyor.MpLink = &Master;
 	Conveyor.Parameters = &ConveyorPar;
 	Conveyor.Enable = 1;
 	
-	ConveyorPar.Velocity = 20;
+	ConveyorPar.Velocity = 55;
 	ConveyorPar.Acceleration = 1000;
 	ConveyorPar.Deceleration = 1000;
 	
+	// Initialization of Slicer MpAxis
 	Slicer.MpLink = &Slave;
 	Slicer.Parameters = &SlicerPar;
 	Slicer.Enable = 1;
@@ -25,11 +26,18 @@ void _INIT ProgramInit(void)
 	Sequencer.Parameters = &SequencerPar;
 	Sequencer.Enable = 1;
 
+	// Initialization of TouchProbe for mark detection
 	TouchProbe.Axis = &Master;
 	TouchProbe.TriggerInput.EventSource = mcEVENT_SRC_TRIGGER1;
-	TouchProbe.Period = 45;
-	TouchProbe.WindowPositive = 40;
+	TouchProbe.Period = 70.0;
+	TouchProbe.WindowNegative = 5.0;
+	TouchProbe.WindowPositive = 5.0;
+	TouchProbe.AdvancedParameters.UseFirstTriggerPosition = 1;
 	TouchProbe.Mode = mcTP_MODE_SHIFT_FROM_RESULT;
+	
+	// Initializatin of TouchProbe for successful cut detection
+	SlicerProbe.Axis = &Slave;
+	SlicerProbe.TriggerInput.EventSource = mcEVENT_SRC_TRIGGER1;
 
 }
 
@@ -57,38 +65,52 @@ void _CYCLIC ProgramCyclic(void)
 	if (Conveyor.IsHomed && Slicer.IsHomed) {
 		Conveyor.MoveVelocity = 1;
 		Sequencer.StartSequence = 1;
+		TouchProbe.Enable = 1;
+		SlicerProbe.Enable = 1;
 	}
 	
+	// If a new hole is detected send a "pulse" to Signal#, else send no pulse.
 	if (TouchProbe.ValidTriggerCount > PrevCounter) {
 		Pulse = 1;
 		
-		switch (Sequencer.ActualStateIndex)
-		{
-			case 0:
-				Sequencer.Signal1 = 1;
-				break;
+		// Evalute current sequencer state, enabled/disable respective signals
+		switch (Sequencer.ActualStateIndex) {
+			// In state 1, enable signal 1 to transition to state 2. Disable signal 3 as a transition-from-state-5 remnant.
 			case 1:
-				Sequencer.Signal4 = 1;
+				Sequencer.Signal1 = 1;
+				Sequencer.Signal3 = 0;
 				break;
+			// In state 2, enable signal 2 to transition to state 3. Disable signal 1 as a transition-from-state-1 remnant.
 			case 2:
-				Sequencer.Signal3 = 1;
+				Sequencer.Signal2 = 1;
+				Sequencer.Signal1 = 0;
 				break;
-			
+			// In state 3, enable signal 3 to transition to state 4. Will bounce back and forth between states 3 and 4.
+			case 3:
+				Sequencer.Signal3 = 1;
+				Sequencer.Signal4 = 0;
+				Sequencer.Signal2 = 0;
+				break;
+			// In state 4, enable signal 4 to transition to state 3.
+			case 4:
+				Sequencer.Signal4 = 1;
+				Sequencer.Signal3 = 0;
+				break;
 		}
 	} else {
 		Pulse = 0;
 	}
-
-	if (Sequencer.ActualStateIndex = 2 && Sequencer.InCompensation) {
-		Sequencer.Signal3 = 0;
-	}
 	
 	// Assignments
+	SuccessfulCuts = SlicerProbe.ValidTriggerCount;
 	PrevCounter = TouchProbe.ValidTriggerCount;
+	
+	// FUBs calls
 	MpAxisBasic(&Conveyor);
 	MpAxisBasic(&Slicer);
 	MpAxisCamSequencer(&Sequencer);
 	MC_BR_TouchProbe(&TouchProbe);
+	MC_BR_TouchProbe(&SlicerProbe);
 }
 
 void _EXIT ProgramExit(void)
