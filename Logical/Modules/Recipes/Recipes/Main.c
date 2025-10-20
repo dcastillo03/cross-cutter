6be2263175d34ca4cfff31629302f26a67efa00f
Daniel Castillo-Idrobo <didrobo@i-automation.com>
2025-10-20 17:08:42 -0400

    improved recipe functionality

--- Logical/Modules/Recipes/Recipes/Main.c
+++ Logical/Modules/Recipes/Recipes/Main.c
@@ -7,12 +7,30 @@
 
 void _INIT ProgramInit(void)
 {
-
+	// MpLink assignments
+	Recipe.Core.MpLink = &gRecipeXml;
+	Recipe.Register.MpLink = &gRecipeXml;
+	Recipe.UI.MpLink = &gRecipeXml;
 }
 
 void _CYCLIC ProgramCyclic(void)
 {
-
+	// FUB Setup
+	Recipe.Core.Enable = 1;
+	Recipe.Core.DeviceName = &"Recipes";
+	Recipe.Core.FileName = &Recipe.Name;
+		
+	Recipe.Register.Enable = 1;
+	Recipe.Register.PVName = &"Manager:TheConveyor";
+		
+	Recipe.UI.Enable = 1;
+	Recipe.UI.DeviceName = &"Recipes";
+	Recipe.UI.UISetup = Recipe.UISetup;
+	Recipe.UI.UIConnect = &Recipe.UIConnect;
+		
+	MpRecipeXml(&Recipe.Core);
+	MpRecipeUI(&Recipe.UI);
+	MpRecipeRegPar(&Recipe.Register);
 }
 
 void _EXIT ProgramExit(void)
--- Logical/Modules/Recipes/Recipes/Types.typ
+++ Logical/Modules/Recipes/Recipes/Types.typ
@@ -1,15 +1,11 @@
 
 TYPE
-	RecipeTyp : 	STRUCT  (*Recipe FUB instances*)
+	RecipeType : 	STRUCT  (*Recipe FUB instances*)
 		Core : MpRecipeXml;
 		Register : MpRecipeRegPar;
 		UI : MpRecipeUI;
 		UISetup : MpRecipeUISetupType;
 		UIConnect : MpRecipeUIConnectType;
 		Name : STRING[50];
-	END_STRUCT;
-	RecipeInternaltyp : 	STRUCT 
-		Cmd : RecipeCmdTyp;
-		Status : RecipeStatusTyp;
 	END_STRUCT;
 END_TYPE
